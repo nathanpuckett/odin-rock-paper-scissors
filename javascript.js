78d3c233efb643f5f02f57131c5944ebb7fb5a21
nathanpuckett <nathanfpuckett@gmail.com>
2022-11-24 19:59:23 -0600

    Complete & test getComputerChoice()

--- javascript.js
+++ javascript.js
@@ -1,10 +1,14 @@
-console.log('Hello World');
-
 function getComputerChoice() {
   const num = Math.floor(Math.random() * 3);
-  
-
-  return num;
+  if (num === 0) {
+    return 'Rock';
+  } else if (num === 1) {
+    return 'Paper';
+  } else if (num === 2) {
+    return 'Scissors';
+  } else {
+    return 'Error';
+  }
 }
 
 for (let i = 0; i < 10; i++) {
