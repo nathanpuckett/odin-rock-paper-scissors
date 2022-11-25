4a6442824f96f4bea98ac42d3908d326e348dc50
nathanpuckett <nathanfpuckett@gmail.com>
2022-11-24 20:16:34 -0600

    Add play()

--- javascript.js
+++ javascript.js
@@ -1,13 +1,45 @@
 function getComputerChoice() {
   const num = Math.floor(Math.random() * 3);
   if (num === 0) {
-    return 'Rock';
+    return 'rock';
   } else if (num === 1) {
-    return 'Paper';
+    return 'paper';
   } else if (num === 2) {
-    return 'Scissors';
+    return 'scissors';
   } else {
-    return 'Error';
+    return 'error';
+  }
+}
+
+function play(playerSelection, computerSelection) {
+  const playerFormatted = playerSelection.toLowerCase();
+
+  if (playerFormatted === 'rock') {
+    if (computerSelection === 'rock') {
+      return "It's a tie! You both chose ROCK";
+    } else if (computerSelection === 'paper') {
+      return "You lose! PAPER beats ROCK";
+    } else if (computerSelection === 'scissors') {
+      return "You win! ROCK beats SCISSORS";
+    }
+  } else if (playerFormatted === 'paper') {
+    if (computerSelection === 'rock') {
+      return "You win! PAPER beats ROCK";
+    } else if (computerSelection === 'paper') {
+      return "It's a tie! You both chose PAPER";
+    } else if (computerSelection === 'scissors') {
+      return "You lose! SCISSORS beats PAPER";
+    }
+  } else if (playerFormatted === 'scissors') {
+    if (computerSelection === 'rock') {
+      return "You lose! ROCK beats SCISSORS";
+    } else if (computerSelection === 'paper') {
+      return "You win! SCISSORS beats PAPER";
+    } else if (computerSelection === 'scissors') {
+      return "It's a tie! You both chose SCISSORS";
+    }
+  } else {
+    return 'error';
   }
 }
 
