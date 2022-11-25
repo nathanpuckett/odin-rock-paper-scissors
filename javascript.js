1fdd552166fdb9c62922dd8d825b497d75b708bc
nathanpuckett <nathanfpuckett@gmail.com>
2022-11-24 18:54:57 -0600

    Add getComputerChoice() function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
 console.log('Hello World');
+
+function getComputerChoice() {
+  const num = Math.floor(Math.random() * 3);
+  
+
+  return num;
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(getComputerChoice());
+}
+
