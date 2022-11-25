1a17e4a493484ef7812608ffe8dcde5af8f6791b
nathanpuckett <nathanfpuckett@gmail.com>
2022-11-24 20:50:51 -0600

    Add game() & finishing touches

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let compScore = 0;
+
 function getComputerChoice() {
   const num = Math.floor(Math.random() * 3);
   if (num === 0) {
@@ -11,29 +14,35 @@ function getComputerChoice() {
   }
 }
 
-function play(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
   const playerFormatted = playerSelection.toLowerCase();
 
   if (playerFormatted === 'rock') {
     if (computerSelection === 'rock') {
       return "It's a tie! You both chose ROCK";
     } else if (computerSelection === 'paper') {
+      compScore += 1;
       return "You lose! PAPER beats ROCK";
     } else if (computerSelection === 'scissors') {
+      playerScore += 1;
       return "You win! ROCK beats SCISSORS";
     }
   } else if (playerFormatted === 'paper') {
     if (computerSelection === 'rock') {
+      playerScore += 1;
       return "You win! PAPER beats ROCK";
     } else if (computerSelection === 'paper') {
       return "It's a tie! You both chose PAPER";
     } else if (computerSelection === 'scissors') {
+      compScore += 1;
       return "You lose! SCISSORS beats PAPER";
     }
   } else if (playerFormatted === 'scissors') {
     if (computerSelection === 'rock') {
+      compScore += 1;
       return "You lose! ROCK beats SCISSORS";
     } else if (computerSelection === 'paper') {
+      playerScore += 1;
       return "You win! SCISSORS beats PAPER";
     } else if (computerSelection === 'scissors') {
       return "It's a tie! You both chose SCISSORS";
@@ -43,7 +52,20 @@ function play(playerSelection, computerSelection) {
   }
 }
 
-for (let i = 0; i < 10; i++) {
-  console.log(getComputerChoice());
+function game() {
+  console.log("Let's play a game...");
+  playerScore = 0;
+  compScore = 0; 
+  for (let i = 0; i < 5; i++) {
+    console.log(playRound(prompt("Rock...Paper...Scissors..."), getComputerChoice()));
+  }
+  if (playerScore > compScore) {
+    alert("You are the winner!");
+  } else if (playerScore === compScore) {
+    alert("You are tied!");
+  } else if (playerScore < compScore) {
+    alert("You are the loser!");
+  }
 }
 
+game();
