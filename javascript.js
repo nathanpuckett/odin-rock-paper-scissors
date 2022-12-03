a5f3dbbe0cb6215af66f0fa97fadc1c824c9bff6
nathanpuckett <nathanfpuckett@gmail.com>
2022-12-03 12:35:24 -0600

    Add <div> to display resuts

--- javascript.js
+++ javascript.js
@@ -19,32 +19,41 @@ function playRound(playerSelection, computerSelection) {
 
   if (playerFormatted === 'rock') {
     if (computerSelection === 'rock') {
+      results.textContent = "It's a tie! You both chose ROCK";
       return "It's a tie! You both chose ROCK";
     } else if (computerSelection === 'paper') {
+      results.textContent = "You lose! PAPER beats ROCK";
       compScore += 1;
       return "You lose! PAPER beats ROCK";
     } else if (computerSelection === 'scissors') {
+      results.textContent = "You win! ROCK beats SCISSORS";
       playerScore += 1;
       return "You win! ROCK beats SCISSORS";
     }
   } else if (playerFormatted === 'paper') {
     if (computerSelection === 'rock') {
+      results.textContent = `You win! PAPER beats ROCK`;
       playerScore += 1;
       return "You win! PAPER beats ROCK";
     } else if (computerSelection === 'paper') {
+      results.textContent = `It's a tie! You both chose PAPER`;
       return "It's a tie! You both chose PAPER";
     } else if (computerSelection === 'scissors') {
+      results.textContent = `You lose! SCISSORS beats PAPER`;
       compScore += 1;
       return "You lose! SCISSORS beats PAPER";
     }
   } else if (playerFormatted === 'scissors') {
     if (computerSelection === 'rock') {
+      results.textContent = `You lose! ROCK beats SCISSORS`;
       compScore += 1;
       return "You lose! ROCK beats SCISSORS";
     } else if (computerSelection === 'paper') {
+      results.textContent = `You win! SCISSORS beats PAPER`;
       playerScore += 1;
       return "You win! SCISSORS beats PAPER";
     } else if (computerSelection === 'scissors') {
+      results.textContent = `It's a tie! You both chose SCISSORS`;
       return "It's a tie! You both chose SCISSORS";
     }
   } else {
@@ -53,7 +62,8 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-  console.log("Let's play a game...");
+  // console.log("Let's play a game...");
+  results.textContent = "Let's play a game...";
   playerScore = 0;
   compScore = 0; 
   /*
@@ -85,5 +95,7 @@ function game() {
     alert("You are the loser!");
   }
 }
+
+const results = document.querySelector('#results');
 
 game();
